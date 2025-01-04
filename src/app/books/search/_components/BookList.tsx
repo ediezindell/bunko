98bc1df34da88a5cc6eb58295af82a9da3150d71
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-01-04 20:39:14 +0900

    refactor: 関数切り出し

--- src/app/books/search/_components/BookList.tsx
+++ src/app/books/search/_components/BookList.tsx
@@ -6,17 +6,15 @@ type Props = {
   books: Item[];
 };
 const BookList = ({ books }: Props) => {
+  const hasBunko = (book: Item) =>
+    books.some(
+      (b) => b.isbn !== book.isbn && b.title === book.title && !isBunko(b),
+    );
+
   return (
     <ul className="flex flex-wrap justify-center gap-4 p-4">
       {books
-        .filter(
-          (book) =>
-            !isBunko(book) ||
-            !books.some(
-              (b) =>
-                b.isbn !== book.isbn && b.title === book.title && !isBunko(b),
-            ),
-        )
+        .filter((book) => !isBunko(book) || !hasBunko(book))
         .map((book) => (
           <li key={book.isbn}>
             <BookCard book={book} />
