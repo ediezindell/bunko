40273da7107d1fe0d00ce7786406406001d9d5f5
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2025-03-27 15:03:03 +0900

    fix: Prisma による登録処理を一旦削除

--- src/app/books/search/_lib/registerNotification.ts
+++ src/app/books/search/_lib/registerNotification.ts
@@ -1,26 +1,13 @@
 'use server';
-import { PrismaClient } from '@prisma/client';
 
 export const registerNotification = async (
   userId: string,
   isbn: string,
   title: string,
 ) => {
-  const prisma = new PrismaClient();
-  await prisma.notifications.upsert({
-    where: {
-      userId_isbn: {
-        userId,
-        isbn,
-      },
-    },
-    create: {
-      userId,
-      isbn,
-      title,
-    },
-    update: {
-      title,
-    },
+  console.log({
+    userId,
+    isbn,
+    title,
   });
 };
